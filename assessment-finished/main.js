8fff70f71a7bd3cf55ad5a7a4a8bbac8ac4620f8
nana-yyy <95006305+nana-yyy@users.noreply.github.com>
2022-02-10 15:35:02 +0900

    Update main.js

--- assessment-finished/main.js
+++ assessment-finished/main.js
@@ -31,7 +31,7 @@ function result() {
     newStory = newStory.replace('Bob', name);
   }
 
-  if (document.getElementById("日本").checked) {
+  if (document.getElementById("").checked) {
     const weight = `${Math.round(300*0.0714286)} kg`;
     const temperature =  `摂氏${Math.round((94-32) * 5 / 9)} 度`;
     newStory = newStory.replace('華氏40度', temperature);
